17e2d2521f4a8d571020d9c2e219a69f765bc563
Willian Martinelli <93338593+yujiwillian@users.noreply.github.com>
2021-10-28 13:33:22 -0300

    Add files via upload

---
+++ Mvc_login/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc_login.Models;
+
+namespace Mvc_login.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (Session["usuarioLogadoID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Usuario u)
+        {
+            // esta action trata o post (login)
+            if (ModelState.IsValid) //verifica se é válido
+            {
+                using (CadastroEntities dc = new CadastroEntities())
+                {
+                    var v = dc.Usuarios.Where(a => a.NomeUsuario.Equals(u.NomeUsuario) && a.Senha.Equals(u.Senha)).FirstOrDefault();
+                    if (v != null)
+                    {
+                        Session["usuarioLogadoID"] = v.Id.ToString();
+                        Session["nomeUsuarioLogado"] = v.NomeUsuario.ToString();
+                        return RedirectToAction("Index");
+                    }
+                }
+            }
+            return View(u);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Macoratti .net";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "http://www.macoratti.net";
+
+            return View();
+        }
+    }
+}
---
+++ Mvc_login/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mvc_login.Startup))]
+namespace Mvc_login
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
